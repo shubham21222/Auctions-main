9707ee6c9fb71b064c162e05d33bd4ab05d09fe9
shubham21222 <shubhamraikwar@hotmail.com>
2025-03-06 17:44:10 +0530

    Chnages in socket

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -13,7 +13,7 @@ export const useSocket = () => {
   useEffect(() => {
     if (!userId || !token) return;
 
-    const socketIo = io("http://localhost:4000", {
+    const socketIo = io("https://bid.nyelizabeth.com/", {
       query: { userId },
       auth: { token },
       transports: ["websocket"],
