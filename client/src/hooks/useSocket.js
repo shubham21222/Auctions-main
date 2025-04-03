9826791e5302034bf6369fc9203a03d30270dee9
shubham21222 <shubhamraikwar@hotmail.com>
2025-04-03 21:02:04 +0530

    Update Socket Connection URL for Consistent Environment Access
    - Changed the socket connection URL in useSocket hook to a hardcoded production URL for improved reliability in connecting to the server.

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -29,7 +29,7 @@ export const useSocket = () => {
     const initializeSocket = () => {
       if (!userId || !token || socket) return;
 
-      socketIo = io(`${config.baseURL}`, {
+      socketIo = io("https://bid.nyelizabeth.com", {
         query: { userId },
         auth: { token, isAdmin: isAdmin || false },
         transports: ["websocket"],
