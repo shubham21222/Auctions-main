9c5cbe7a87f57e499fe8f18851cfef3fb348d0df
Harshal <harshalpawar013@gmail.com>
2025-04-12 19:58:55 +0530

    add

--- server/src/v1/api/config/stripeConfig.js
+++ server/src/v1/api/config/stripeConfig.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-const stripe = new Stripe("sk_test_51PAs60SB7WwtOtybywUDUG9MdtdWGlWHMww0HcmcYxKH1Odx4US1PhizF5mrg5ihlNbE85KwgVv51SYYCXQU1NRU00zSlwYANZ", {
+const stripe = new Stripe(process.env.STRIPE_KEY, {
     apiVersion: "2023-10-16",
   });
 
--- server/src/v1/api/services/Stripe/stripe.service.js
+++ server/src/v1/api/services/Stripe/stripe.service.js
@@ -1,9 +1,10 @@
 import Stripe from 'stripe';
-
+import dotenv from 'dotenv';
+dotenv.config();
 class StripeService {
     
   constructor() {
-    this.stripe = new Stripe("sk_test_51PAs60SB7WwtOtybywUDUG9MdtdWGlWHMww0HcmcYxKH1Odx4US1PhizF5mrg5ihlNbE85KwgVv51SYYCXQU1NRU00zSlwYANZ");
+    this.stripe = new Stripe(process.env.STRIPE_KEY);
   }
 
   async findOrCreateCustomer(email, name) {
